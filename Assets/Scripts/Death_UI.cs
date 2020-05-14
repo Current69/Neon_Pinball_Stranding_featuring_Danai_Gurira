27961abb4aa6b1de37fffaf537cd7454a85238db
Dan Miranda <danmiranda3000@gmai.com>
2020-05-13 19:44:33 -0700

    redo of last commit. death, reset, exit, fance, blah blah, etc.

---
+++ Assets/Scripts/Death_UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Death_UI : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    public Text deathText;
+    public GameObject restartLoseButton;
+    public GameObject exitLoseButton;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+		{
+            gameOverCanvas.gameObject.SetActive(true);
+            deathText.enabled = true;
+            restartLoseButton.gameObject.SetActive(true);
+            exitLoseButton.gameObject.SetActive(true);
+			Destroy(other.gameObject);
+		}
+    }
+}
