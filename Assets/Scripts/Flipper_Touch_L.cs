30ea1681293aec2a4ce101d550e951df127214c6
Dan Miranda <danmiranda3000@gmai.com>
2020-05-25 17:17:06 -0700

    fixing last commit. mouse controls, bumper

---
+++ Assets/Scripts/Flipper_Touch_L.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper_Touch_L : MonoBehaviour
+{
+    public float restPosition = 0f;
+    public float pressedPosition = -45f;
+    public float hitStrength = 50000f;
+    public float flipperDamper = 150f;
+    
+    HingeJoint hinge;
+
+    public string inputName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+
+        if (Input.GetAxis(inputName) == 1)
+        {
+            if(Input.mousePosition.x < 353f)
+            {
+                spring.targetPosition = pressedPosition;
+                Debug.Log (Input.mousePosition.x);
+            }
+        }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
+    }
+}
