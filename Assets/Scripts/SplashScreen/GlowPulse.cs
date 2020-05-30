4538a305b3bd547e363306110bb76dee1d6f330b
Dan Miranda <danmiranda3000@gmai.com>
2020-05-29 20:59:59 -0700

    added animated, functional splash screen

---
+++ Assets/Scripts/SplashScreen/GlowPulse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowPulse : MonoBehaviour
+{
+    public Light glowLight;
+    public float minRange = 4.8f;
+    public float maxRange = 8.3f;
+    public float rangeSpeed = 1.0f;
+    public float intensitySpeed = 0.1f;
+    public float minIntensity = 1.0f;
+    public float maxIntensity = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        glowLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        glowLight.range = minRange + Mathf.PingPong(Time.time * rangeSpeed, maxRange - minRange);
+        glowLight.intensity = minIntensity + Mathf.PingPong(Time.time * intensitySpeed, maxIntensity - minIntensity);
+    }
+}
